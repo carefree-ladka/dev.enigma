ce953ee8cb7aefa0a2275bcd647e4cd867584309
Pawan Kumar <pawan.kumar@compassdigital.io>
2025-08-16 23:29:02 +0530

    Added Java notes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,20 +10,15 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/js.enigma/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jsenigma', // Usually your GitHub org/user name.
   projectName: 'js.enigma', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -35,26 +30,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/carefree-ladka',
+          editUrl: 'https://github.com/carefree-ladka',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   feedOptions: {
-        //     type: ['rss', 'atom'],
-        //     xslt: true,
-        //   },
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        //   // Useful options to enforce blogging best practices
-        //   onInlineTags: 'warn',
-        //   onInlineAuthors: 'warn',
-        //   onUntruncatedBlogPosts: 'warn',
-        // },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -63,7 +40,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'JSEnigma',
@@ -78,7 +54,6 @@ const config: Config = {
           position: 'left',
           label: 'Learning',
         },
-        // { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://www.linkedin.com/in/kumpawan/',
           label: 'LinkedIn',
@@ -91,49 +66,27 @@ const config: Config = {
       links: [
         {
           title: 'Docs',
-          items: [
-            {
-              label: 'Learning',
-              to: '/docs/intro',
-            },
-          ],
+          items: [{ label: 'Learning', to: '/docs/intro' }],
         },
         {
           title: 'Community',
           items: [
-            {
-              label: 'Instagram',
-              href: 'https://www.instagram.com/carefree_ladka/'
-            },
-            {
-              label: 'LeetCode',
-              href: 'https://leetcode.com/u/UP41Guy/',
-            },
-            {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/in/kumpawan/',
-            },
+            { label: 'Instagram', href: 'https://www.instagram.com/carefree_ladka/' },
+            { label: 'LeetCode', href: 'https://leetcode.com/u/UP41Guy/' },
+            { label: 'LinkedIn', href: 'https://www.linkedin.com/in/kumpawan/' },
           ],
         },
         {
           title: 'More',
-          items: [
-            // {
-            //   label: 'Blog',
-            //   to: '/blog',
-            // },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/carefree-ladka',
-            },
-          ],
+          items: [{ label: 'GitHub', href: 'https://github.com/carefree-ladka' }],
         },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} JSEnigma, Built with Kadak Chai`,
     },
     prism: {
-      theme: prismThemes.oneLight,
-      darkTheme: prismThemes.oneDark,
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java'], // 👈 Fix for Java syntax highlighting
     },
   } satisfies Preset.ThemeConfig,
 };
